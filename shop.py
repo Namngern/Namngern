7f6d0b8c0226333eca64e3cd12050d0bbcbf12f3
Namngern <88497229+Namngern@users.noreply.github.com>
2021-10-18 22:48:24 +0700

    Update shop.py

--- shop.py
+++ shop.py
@@ -1,1 +1,27 @@
-นนท์เท่
+Assasin Shop (น้ำเงิน)
+ชุด
+-- หมวก
+  Glitchpop
+  Elderflame
+  Inferno
+-- เกราะ
+  Embers
+  Sentinel
+  Excelsior
+  Doom's
+-- กางเกง
+  Lunar
+  Navy
+  Verdant
+-- รองเท้า
+  Hellborn
+  Demonic
+  Mirage
+อาวุธ
+-- มีดสั้น
+  Karambit
+  Butterfly Knife
+  Kunai
+  Reaver Knife
+  Singularity Knife
+ 
